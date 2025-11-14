01d0e5c1954b4f6d9332af02622167fe20b4a765
TakshakAmbekar <ambekartakshak@gmail.com>
2025-11-14 22:04:33 +0530

    Day 14

---
+++ Problems/Day 14/CF round 184 (Educational)/C_Range_Operation.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* clang-format off */
+
+#define Takshak ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl "\n"
+
+/* TYPES  */
+#define ll long long
+#define lll __int128
+#define pi pair<int, int>
+#define pl pair<long long, long long>
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+
+/* FUNCTIONS */
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define fe(i,c) for(auto &i : c)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define eb emplace_back
+
+/* READS */
+template <typename T>
+T read() { T x; cin >> x; return x; }
+
+#define readi() read<int>()
+#define readl() read<long long>()
+#define reads() read<string>()
+
+#define readvi(n) ({ vector<int> v(n); for(auto &x:v) cin >> x; v; })
+#define readvl(n) ({ vector<long long> v(n); for(auto &x:v) cin >> x; v; })
+#define readvs(n) ({ vector<string> v(n); for(auto &x:v) cin >> x; v; })
+
+/* PRINTS */
+template <class T>
+void print(const T& x, int space = 0) { cout << x; if(space == 0) cout << " "; if(space == 1) cout << "\n"; }
+
+template <class T>
+void print(const vector<T>& v) { for(size_t i = 0; i < v.size(); i++) { if(i < v.size() - 1) print(v[i]); else print(v[i], 1); }}
+
+template <class T1, class T2>
+void print(const pair<T1,T2>& p) { print(p.first); print(p.second); cout << "\n"; }
+
+/* UTILS */
+#define MOD 1000000007
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a*b/gcd(a,b); }
+bool is_prime(ll x) { if (x==1) return 0; for(int i = 2; i*i <= x; ++i) if (x%i==0) return 0; return 1; }
+
+/* DEBUGS */
+#define dbg(x) cout << #x << " = " << x << endl;
+#define dbg_1D(v) cout << #v << ": "; fe(x, v){print(x);} cout << endl;
+#define dbg_map(m) cout << #m << ": " << endl; fe(x, m){cout << x.first << " " << x.second << endl;}
+
+
+//----------------------------------------------------------------------------------------------------
+
+/*---------Given Data---------
+
+*/
+
+/*---------Objective---------
+
+*/
+
+/*---------Constraints---------
+
+*/
+
+/*---------Observations---------
+
+*/
+
+/*---------Intuition---------
+
+*/
+
+/*---------Claims---------
+
+*/
+
+/*---------Approach---------
+
+*/
+
+// Time taken to solve : 
+// Hints used : 
+//----------------------------------------------------------------------------------------------------
+
+ll solve() {
+    int n = readi();
+    vl values = readvl(n);
+
+    vl pref(n + 1, 0);
+    cf(i, 1, n)
+        pref[i] = pref[i - 1] + values[i - 1];
+    
+
+    ll total = pref[n];
+    ll best = total;      
+
+    vl rightContribution(n + 1);
+    cf(r, 1, n)
+        rightContribution[r] = 1LL * r * r + r - pref[r];
+    
+
+    vl suf(n + 2, LLONG_MIN);
+    suf[n] = rightContribution[n];
+
+    rf(i, n, 1)
+        suf[i] = max(suf[i + 1], rightContribution[i]);
+
+    cf(l, 1, n){
+        ll leftContribution = pref[l - 1] - 1LL * l * l + l;                  
+        ll bestRight = suf[l];
+        ll newTotal = total + leftContribution + bestRight;
+        best = max(best, newTotal);
+    }
+
+    return best;
+}
+
+
+
+
+int main() {
+    Takshak
+    ll t = readl();
+    while(t--) {
+        ll ans = solve();
+        print(ans, 1);
+    }
+    return 0;
+}
